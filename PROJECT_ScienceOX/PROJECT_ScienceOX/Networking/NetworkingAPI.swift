af140d12b53fbc299bd438a85515b73b9680656a
jangseoyoung <jangseoyoung0402@gmail.com>
2021-01-04 23:22:39 +0900

    Update : NetworkingAPI

--- PROJECT_ScienceOX/PROJECT_ScienceOX/Networking/NetworkingAPI.swift
+++ PROJECT_ScienceOX/PROJECT_ScienceOX/Networking/NetworkingAPI.swift
@@ -7,3 +7,37 @@
 
 import Foundation
 import Alamofire
+
+enum NetworkingAPI{
+    case ProvideQuiz(_ question : String)
+    case Check(_ correct : String)
+    
+    var path : String{
+        switch self {
+        case .ProvideQuiz:
+            return "/question"
+        case .Check:
+            return "/question/check?id=1&answer=1"
+        }
+    }
+    
+    var headers : HTTPHeaders?{
+        switch self {
+        case .Check, .ProvideQuiz:
+            return nil
+        }
+    }
+    
+    var parameters : [String:Any]{
+        switch self {
+        case .ProvideQuiz(let question):
+            print(["qusetion" : question])
+            return ["question" : question]
+        case .Check(let correct):
+            print(["correct" : correct])
+            return ["correct" : correct]
+        default:
+            return [:]
+        }
+    }
+}
