cc9a7a554567993e19b0c9a456c93f9f2d7d20ec
jangseoyoung <jangseoyoung0402@gmail.com>
2021-01-05 23:18:41 +0900

    Create : ProvideQuizModel
    in Model Folder

---
+++ PROJECT_ScienceOX/PROJECT_ScienceOX/Model/ProvideQuizModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProvideQuizModel.swift
+//  PROJECT_ScienceOX
+//
+//  Created by 장서영 on 2021/01/05.
+//
+
+import Foundation
+import Alamofire
+
+struct ProvideQuizModel : Codable {
+    var question : String
+}
