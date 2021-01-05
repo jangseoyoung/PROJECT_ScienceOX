d8f7cf324e1de0bb6efec8afae86a90c4a8793d4
jangseoyoung <jangseoyoung0402@gmail.com>
2021-01-05 23:16:12 +0900

    Create : CheckModel
    in Model Folder

---
+++ PROJECT_ScienceOX/PROJECT_ScienceOX/Model/CheckModel.swift
@@ -0,0 +1,13 @@
+//
+//  CheckModel.swift
+//  PROJECT_ScienceOX
+//
+//  Created by 장서영 on 2021/01/05.
+//
+
+import Foundation
+import Alamofire
+
+struct CheckModel : Codable {
+    var correct : String
+}
