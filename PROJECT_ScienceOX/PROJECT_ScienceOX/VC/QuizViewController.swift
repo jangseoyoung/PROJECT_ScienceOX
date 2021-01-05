f63582fa90454b10a2d97d31ba8e6e9ec5f37d44
jangseoyoung <jangseoyoung0402@gmail.com>
2021-01-05 23:19:01 +0900

    Update : QuizViewController

--- PROJECT_ScienceOX/PROJECT_ScienceOX/VC/QuizViewController.swift
+++ PROJECT_ScienceOX/PROJECT_ScienceOX/VC/QuizViewController.swift
@@ -6,8 +6,21 @@
 //
 
 import UIKit
+import Alamofire
 
 class QuizViewController: UIViewController {
+    
+    let httpclient = HTTPClient()
+    private var getmodel : ProvideQuizModel?
+    private var postmodel : CheckModel?
+    
+    @IBAction func correctButton(_ sender : UIButton){
+        
+    }
+    
+    @IBAction func incorrectButton(_ sender : UIButton){
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +28,45 @@ class QuizViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
+    func CheckAnswer(correct : String){
+        httpclient.post(.Check(correct)).responseJSON(completionHandler: {(response) in
+            switch response.response?.statusCode{
+            case 200:
+                print("채점 성공")
+                guard let data = response.data else {return}
+                guard let model = try? JSONDecoder().decode(CheckModel.self, from: data) else {return}
+                self.postmodel = model
+                
+                if model.correct == "1"{
+                    let alert = UIAlertController(title: "정답입니다!", message: nil, preferredStyle: UIAlertController.Style.alert)
+                    let cancelAction = UIAlertAction(title: "확인", style: .cancel, handler: nil)
+                    alert.addAction(cancelAction)
+                    self.present(alert, animated: true, completion: nil)
+                }
+                
+                else if model.correct == "0"{
+                    let alert = UIAlertController(title: "틀렸습니다!", message: nil, preferredStyle: UIAlertController.Style.alert)
+                    let cancelAction = UIAlertAction(title: "확인", style: .cancel, handler: nil)
+                    alert.addAction(cancelAction)
+                    self.present(alert, animated: true, completion: nil)
+                }
+                
+                else {
+                    print("오류입니다.")
+                }
+                
+            case 400:
+                print("BAD REQUEST")
+            case 404:
+                print("NOT FOUND")
+            default:
+                print("알 수 없는 오류")
+                
+            }
+        })
+    }
+
 
     /*
     // MARK: - Navigation
