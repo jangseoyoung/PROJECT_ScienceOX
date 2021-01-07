b8864c2ca3e2a72f99a97e36e08a5f4b7d5fd638
jangseoyoung <jangseoyoung0402@gmail.com>
2021-01-07 23:04:05 +0900

    Update : HTTPclient

--- PROJECT_ScienceOX/PROJECT_ScienceOX/Networking/HTTPclient.swift
+++ PROJECT_ScienceOX/PROJECT_ScienceOX/Networking/HTTPclient.swift
@@ -18,18 +18,18 @@ class HTTPClient: HTTPClientProvider {
     let baseip = "http://10.156.147.203:1"
     
     func get(_ api: NetworkingAPI) -> DataRequest {
-        print(baseip + api.path)
-        print(api.parameters)
-        print(api.headers)
+//        print(baseip + api.path)
+//        print(api.parameters)
+//        print(api.headers)
         
         return AF.request(baseip + api.path, method: .get, parameters: api.parameters, encoding: JSONEncoding.prettyPrinted,
                           headers: api.headers, interceptor: nil)
     }
     
     func post(_ api: NetworkingAPI) -> DataRequest {
-        print(baseip + api.path)
-        print(api.parameters)
-        print(api.headers)
+//        print(baseip + api.path)
+//        print(api.parameters)
+//        print(api.headers)
         
         return AF.request(baseip + api.path, method: .post, parameters: api.parameters, encoding: JSONEncoding.prettyPrinted,
                           headers: api.headers, interceptor: nil)
