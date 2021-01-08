e704c555195b28df95969c84f8f9fdf9fa5eef2b
jangseoyoung <jangseoyoung0402@gmail.com>
2021-01-08 23:58:08 +0900

    Update : HTTPclient

--- PROJECT_ScienceOX/PROJECT_ScienceOX/Networking/HTTPclient.swift
+++ PROJECT_ScienceOX/PROJECT_ScienceOX/Networking/HTTPclient.swift
@@ -10,12 +10,11 @@ import Alamofire
 
 protocol HTTPClientProvider {
     func get(_ api: NetworkingAPI) -> DataRequest
-    func post(_ api: NetworkingAPI) -> DataRequest
 }
 
 class HTTPClient: HTTPClientProvider {
     
-    let baseip = "http://10.156.147.203:1"
+    let baseip = "http://scienceox.herokuapp.com"
     
     func get(_ api: NetworkingAPI) -> DataRequest {
 //        print(baseip + api.path)
@@ -25,16 +24,6 @@ class HTTPClient: HTTPClientProvider {
         return AF.request(baseip + api.path, method: .get, parameters: api.parameters, encoding: JSONEncoding.prettyPrinted,
                           headers: api.headers, interceptor: nil)
     }
-    
-    func post(_ api: NetworkingAPI) -> DataRequest {
-//        print(baseip + api.path)
-//        print(api.parameters)
-//        print(api.headers)
-        
-        return AF.request(baseip + api.path, method: .post, parameters: api.parameters, encoding: JSONEncoding.prettyPrinted,
-                          headers: api.headers, interceptor: nil)
-    }
-    
     
     
 }
