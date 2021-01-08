e75d20e8b90d185396bc851227aa2f9aa46a2bde
jangseoyoung <jangseoyoung0402@gmail.com>
2021-01-08 23:57:59 +0900

    Update : NetworkingAPI

--- PROJECT_ScienceOX/PROJECT_ScienceOX/Networking/NetworkingAPI.swift
+++ PROJECT_ScienceOX/PROJECT_ScienceOX/Networking/NetworkingAPI.swift
@@ -9,32 +9,26 @@ import Foundation
 import Alamofire
 
 enum NetworkingAPI{
-    case ProvideQuiz(_ question : String)
-    case Check(_ correct : String)
+    case ProvideQuiz(_ question : String, isCorrect : String)
     
     var path : String{
         switch self {
         case .ProvideQuiz:
             return "/question"
-        case .Check:
-            return "/question/check?id=1&answer=1"
         }
     }
     
     var headers : HTTPHeaders?{
-        switch self {
-        case .Check, .ProvideQuiz:
-            return nil
-        }
+        return nil
     }
     
     var parameters : [String:Any]{
         switch self {
-        case .Check(let correct):
-            print(["correct" : correct])
-            return ["correct" : correct]
+        case .ProvideQuiz(let question, let isCorrect):
+            return [:]
         default:
             return [:]
         }
     }
 }
+
